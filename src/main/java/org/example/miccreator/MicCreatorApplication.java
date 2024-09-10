76bd34ec46b1dccb39f9bcae175705dc9437a190
dliub <dliub@softserveinc.com>
2024-09-10 16:11:24 +0300

    initial commit

---
+++ src/main/java/org/example/miccreator/MicCreatorApplication.java
@@ -0,0 +1,13 @@
+package org.example.miccreator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicCreatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicCreatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/miccreator/MicCreatorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.miccreator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicCreatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
